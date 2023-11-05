e4a45adb5e9f82c8de2fff12666719c9a77b9530
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-06 00:10:24 +0100

    fix: typescript with arrays

---
+++ myArrays.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeros = [];
+// const heroPower: number[] = [];
+var heroPower = [];
+var allUsers = [];
+var MLModels = [
+    [255, 255, 255],
+    [196, 197, 197],
+];
+superHeros.push('spiderman');
+heroPower.push(2);
+allUsers.push({ name: 'John', isActive: true });
