3ceb6381f9d61c4a76e0750a54fe57bd1d32a89b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-05 05:44:08 +0100

    feat: intro to variable declaration

---
+++ variableMe.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = 'Hello John';
+var myNum = 6;
+greetings.toUpperCase();
+console.log(greetings);
---
+++ variableMe.ts
@@ -0,0 +1,8 @@
+let greetings: string = 'Hello John';
+
+let myNum = 6;
+
+greetings.toUpperCase();
+console.log(greetings);
+
+export {};
