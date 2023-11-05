28da508536e21b6440864ad08fba58d62e43c394
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-05 05:36:39 +0100

    first commit

---
+++ intro.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = {
+    name: 'John',
+    age: 18,
+};
+console.log('John');
+console.log(user.name);
+console.log(user.email);
