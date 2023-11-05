f524a99202fd6fb77161c625aeb53c46a09f996a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-05 18:07:52 +0100

    fix: handle typescript with functions

--- myFunctions.js
+++ myFunctions.js
@@ -15,3 +15,24 @@ var loginUser = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
 };
 loginUser('John', 'john@example.com');
+function getValue(myVal) {
+    if (myValue > 5) {
+        return true;
+    }
+    return '200 OK';
+}
+getValue(1);
+var getHello = function (s) {
+    return '';
+};
+var heros = ['thor', 'spiderman', 'ironman'];
+// const heros = [1, 2, 3];
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
--- myFunctions.ts
+++ myFunctions.ts
@@ -1,4 +1,4 @@
-function addTwo(num: number) {
+function addTwo(num: number): number {
   return num + 2;
 }
 
@@ -18,5 +18,33 @@ signUpUser('John', 'john@example.com', false);
 const loginUser = (name: string, email: string, isPaid: boolean = false) => {};
 
 loginUser('John', 'john@example.com');
+
+function getValue(myVal: number): boolean | string {
+  if (myValue > 5) {
+    return true;
+  }
+  return '200 OK';
+}
+
+getValue(1);
+
+const getHello = (s: string): string => {
+  return '';
+};
+
+const heros = ['thor', 'spiderman', 'ironman'];
+// const heros = [1, 2, 3];
+
+heros.map((hero): string => {
+  return `hero is ${hero}`;
+});
+
+function consoleError(errmsg: string): void {
+  console.log(errmsg);
+}
+
+function handleError(errmsg: string): never {
+  throw new Error(errmsg);
+}
 
 export {};
