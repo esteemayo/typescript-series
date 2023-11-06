329b945f78b36924823be4d618300c7a7f2b0078
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-06 17:01:51 +0100

    fix: comment out

--- purets/dist/main.js
+++ purets/dist/main.js
@@ -1,19 +1,27 @@
 "use strict";
-class Instagram {
-  constructor(cameraMode, filter, burst) {
-    this.cameraMode = cameraMode;
-    this.filter = filter;
-    this.burst = burst;
-  }
-}
-class Youtube {
-  constructor(cameraMode, filter, burst, short) {
-    this.cameraMode = cameraMode;
-    this.filter = filter;
-    this.burst = burst;
-    this.short = short;
-  }
-  createStory() {
-    console.log('Story was created');
-  }
-}
+// interface TakePhoto {
+//   cameraMode: string;
+//   filter: string;
+//   burst: number;
+// }
+// interface Story {
+//   createStory(): void;
+// }
+// class Instagram implements TakePhoto {
+//   constructor(
+//     public cameraMode: string,
+//     public filter: string,
+//     public burst: number
+//   ) {}
+// }
+// class Youtube implements TakePhoto, Story {
+//   constructor(
+//     public cameraMode: string,
+//     public filter: string,
+//     public burst: number,
+//     public short: string
+//   ) {}
+//   createStory(): void {
+//     console.log('Story was created');
+//   }
+// }
--- purets/src/main.ts
+++ purets/src/main.ts
@@ -1,30 +1,30 @@
-interface TakePhoto {
-  cameraMode: string;
-  filter: string;
-  burst: number;
-}
+// interface TakePhoto {
+//   cameraMode: string;
+//   filter: string;
+//   burst: number;
+// }
 
-interface Story {
-  createStory(): void;
-}
+// interface Story {
+//   createStory(): void;
+// }
 
-class Instagram implements TakePhoto {
-  constructor(
-    public cameraMode: string,
-    public filter: string,
-    public burst: number
-  ) {}
-}
+// class Instagram implements TakePhoto {
+//   constructor(
+//     public cameraMode: string,
+//     public filter: string,
+//     public burst: number
+//   ) {}
+// }
 
-class Youtube implements TakePhoto, Story {
-  constructor(
-    public cameraMode: string,
-    public filter: string,
-    public burst: number,
-    public short: string
-  ) {}
+// class Youtube implements TakePhoto, Story {
+//   constructor(
+//     public cameraMode: string,
+//     public filter: string,
+//     public burst: number,
+//     public short: string
+//   ) {}
 
-  createStory(): void {
-    console.log('Story was created');
-  }
-}
+//   createStory(): void {
+//     console.log('Story was created');
+//   }
+// }
