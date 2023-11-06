67db7e63b53a3b18999cfeee9f4c50d06d72bfc5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-06 03:50:57 +0100

    fix: change directory

--- intro.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var user = {
-    name: 'John',
-    age: 18,
-};
-console.log('John');
-console.log(user.name);
-console.log(user.email);
