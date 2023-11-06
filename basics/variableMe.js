d20cc07614b0df11393df90bd6843bf387622a34
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-06 03:53:46 +0100

    fix: change directory

---
+++ basics/variableMe.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = 'Hello John';
+greetings.toUpperCase();
+console.log(greetings);
+// number
+var userId = 334455.3;
+userId.toFixed(2);
+// boolean
+var isLoggedIn = false;
+// any
+var hero;
+function getHero() {
+    return 'thor';
+}
+hero = getHero();
