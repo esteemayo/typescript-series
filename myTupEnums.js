7c2e3635d87d4adb8eb77cc934792b4f5b0b4b1a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-06 03:52:10 +0100

    fix: change directory

--- myTupEnums.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var user;
-user = ['john', 1, true];
-var rgb = [255, 123, 112];
-var newUser = [112, 'test@example.com'];
-newUser[1] = 'john@example.com';
