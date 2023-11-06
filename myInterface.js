632e5224039a75f6c4e82a41e6de2459f47527b0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-06 03:49:05 +0100

    fix: interfaces in typescript

--- myInterface.js
+++ myInterface.js
@@ -1,15 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var john = {
-    dbId: 22,
-    email: 'john@example.com',
-    userId: 2211,
-    role: 'admin',
-    githubToken: 'github',
-    startTrial: function () {
-        return 'trial started';
-    },
-    getCoupon: function (name, off) {
-        return 10;
-    },
+  dbId: 22,
+  email: 'john@example.com',
+  userId: 2211,
+  role: 'admin',
+  githubToken: 'github',
+  startTrial: function () {
+    return 'trial started';
+  },
+  getCoupon: function (name, off) {
+    return 10;
+  },
 };
