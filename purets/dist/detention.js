69ebaa06014b1cbb47d2baf75b06c26702b117a0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-07 22:39:06 +0100

    fix: the never and exhaustiveness checking in typescript

--- purets/dist/detention.js
+++ purets/dist/detention.js
@@ -54,5 +54,18 @@ function getTrueShape(shape) {
   if (shape.kind === 'circle') {
     return Math.PI * shape.radius ** 2;
   }
-  return shape.side;
+  // return shape.side * shape.side;
+}
+function getArea(shape) {
+  switch (shape.kind) {
+    case 'circle':
+      return Math.PI * shape.radius ** 2;
+    case 'square':
+      return shape.side * shape.side;
+    case 'rectangle':
+      return shape.length * shape.width;
+    default:
+      const _exhaustiveCheck = shape;
+      return _exhaustiveCheck;
+  }
 }
--- purets/src/detention.ts
+++ purets/src/detention.ts
@@ -83,11 +83,28 @@ interface Rectangle {
   width: number;
 }
 
-type Shape = Circle | Square;
+type Shape = Circle | Square | Rectangle;
 
 function getTrueShape(shape: Shape) {
   if (shape.kind === 'circle') {
     return Math.PI * shape.radius ** 2;
   }
-  return shape.side;
+  // return shape.side * shape.side;
+}
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case 'circle':
+      return Math.PI * shape.radius ** 2;
+
+    case 'square':
+      return shape.side * shape.side;
+
+    case 'rectangle':
+      return shape.length * shape.width;
+
+    default:
+      const _exhaustiveCheck: never = shape;
+      return _exhaustiveCheck;
+  }
 }
