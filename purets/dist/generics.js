be9a34527f065c42c81d6db72d8a19298b9072e4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-07 16:37:02 +0100

    fix: modify

--- purets/dist/generics.js
+++ purets/dist/generics.js
@@ -2,41 +2,41 @@
 const score = [];
 const names = [];
 function identityOne(val) {
-  return val;
+    return val;
 }
 function identityTwo(val) {
-  return val;
+    return val;
 }
 function identityThree(val) {
-  return val;
+    return val;
 }
 identityThree(3);
 function identityFour(val) {
-  return val;
+    return val;
 }
 identityFour({ brand: '', type: '' });
 function getSearchProducts(products) {
-  // do some database operations
-  const myIndex = 3;
-  return products[myIndex];
+    // do some database operations
+    const myIndex = 3;
+    return products[myIndex];
 }
 const getMoreSearchProducts = (products) => {
-  // do some database operations
-  const myIndex = 4;
-  return products[myIndex];
+    // do some database operations
+    const myIndex = 4;
+    return products[myIndex];
 };
 function anotherFunction(valOne, valTwo) {
-  return {
-    valOne,
-    valTwo,
-  };
+    return {
+        valOne,
+        valTwo,
+    };
 }
 anotherFunction(3, { connection: '', username: '', password: '' });
 class Sellable {
-  constructor() {
-    this.cart = [];
-  }
-  addToCart(product) {
-    this.cart.push(product);
-  }
+    constructor() {
+        this.cart = [];
+    }
+    addToCart(product) {
+        this.cart.push(product);
+    }
 }
