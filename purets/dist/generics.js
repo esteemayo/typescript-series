77c7592286bc664a0979bb605d5f3432a4f43b97
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-07 15:26:13 +0100

    fix: modify

--- purets/dist/generics.js
+++ purets/dist/generics.js
@@ -2,16 +2,16 @@
 const score = [];
 const names = [];
 function identityOne(val) {
-    return val;
+  return val;
 }
 function identityTwo(val) {
-    return val;
+  return val;
 }
 function identityThree(val) {
-    return val;
+  return val;
 }
 identityThree(3);
 function identityFour(val) {
-    return val;
+  return val;
 }
 identityFour({ brand: '', type: '' });
