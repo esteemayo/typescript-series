79abddae0d53ae035178237de22b573ee969e30d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-07 15:46:25 +0100

    fix: generics in typescript

--- purets/dist/generics.js
+++ purets/dist/generics.js
@@ -15,3 +15,13 @@ function identityFour(val) {
   return val;
 }
 identityFour({ brand: '', type: '' });
+function getSearchProducts(products) {
+  // do some database operations
+  const myIndex = 3;
+  return products[myIndex];
+}
+const getMoreSearchProducts = (products) => {
+  // do some database operations
+  const myIndex = 4;
+  return products[myIndex];
+};
--- purets/src/generics.ts
+++ purets/src/generics.ts
@@ -25,3 +25,15 @@ interface Bottle {
 }
 
 identityFour<Bottle>({ brand: '', type: '' });
+
+function getSearchProducts<T>(products: T[]): T {
+  // do some database operations
+  const myIndex = 3;
+  return products[myIndex];
+}
+
+const getMoreSearchProducts = <T>(products: T[]): T => {
+  // do some database operations
+  const myIndex = 4;
+  return products[myIndex];
+};
