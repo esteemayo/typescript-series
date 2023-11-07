11720f0140c5acb8186950adbc4b0671afda2ccc
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-07 16:37:13 +0100

    fix: narrowing in typescript

---
+++ purets/dist/detention.js
@@ -0,0 +1,26 @@
+"use strict";
+function detectType(val) {
+  if (typeof val === 'string') {
+    return val.toLowerCase();
+  }
+  return val + 3;
+}
+function provideId(id) {
+  if (!id) {
+    console.log('Please provide ID');
+    return;
+  }
+  return id.toLowerCase();
+}
+function printAll(strs) {
+  if (strs) {
+    if (typeof strs === 'object') {
+      for (const s of strs) {
+        console.log(s);
+      }
+    }
+    else if (typeof strs === 'string') {
+      console.log(strs);
+    }
+  }
+}
---
+++ purets/src/detention.ts
@@ -0,0 +1,26 @@
+function detectType(val: number | string) {
+  if (typeof val === 'string') {
+    return val.toLowerCase();
+  }
+  return val + 3;
+}
+
+function provideId(id: string | null) {
+  if (!id) {
+    console.log('Please provide ID');
+    return;
+  }
+  return id.toLowerCase();
+}
+
+function printAll(strs: string | string[] | null) {
+  if (strs) {
+    if (typeof strs === 'object') {
+      for (const s of strs) {
+        console.log(s);
+      }
+    } else if (typeof strs === 'string') {
+      console.log(strs);
+    }
+  }
+}
