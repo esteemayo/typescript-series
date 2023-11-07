36274c46efae44974537ec465301361b5c0576fa
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-07 18:16:12 +0100

    fix: type predicate narrowing in typescript

--- purets/dist/detention.js
+++ purets/dist/detention.js
@@ -29,3 +29,24 @@ function isAdminAccount(account) {
     return account.isAdmin;
   }
 }
+function logValue(x) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  }
+  else {
+    console.log(x.toUpperCase());
+  }
+}
+function isFish(pet) {
+  return pet.swim !== undefined;
+}
+function getFood(pet) {
+  if (isFish(pet)) {
+    pet;
+    return 'Fish Food';
+  }
+  else {
+    pet;
+    return 'Bird Food';
+  }
+}
--- purets/src/detention.ts
+++ purets/src/detention.ts
@@ -41,3 +41,28 @@ function isAdminAccount(account: IUser | IAdmin) {
     return account.isAdmin;
   }
 }
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return 'Fish Food';
+  } else {
+    pet;
+    return 'Bird Food';
+  }
+}
